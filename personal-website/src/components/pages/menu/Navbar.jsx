610bb2ee47dd77a1f78dec3e2d544ced50104cad
Matthew <mattghanno@gmail.com>
2020-03-19 11:04:24 +1100

    Added homepage to package.json, modified the website to use HashRouter and changed the css for panel, infoPanel and gitItem so height is determined by the browser not set to 75vh

--- personal-website/src/components/pages/menu/Navbar.jsx
+++ personal-website/src/components/pages/menu/Navbar.jsx
@@ -3,16 +3,16 @@ import React from "react";
 function Navbar(props) {
 
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark dark-green stuck" style={{"height": "6.5vh"}}>
+        <nav className="navbar navbar-expand-sm navbar-dark dark-green stuck">
             <a className="navbar-brand" href="/">MG</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav mx-auto">
-                    <a className={ props.pagesActive[0] ? "nav-item nav-link active" : "nav-item nav-link" } href="/">Home <span className="sr-only">(current)</span></a>
-                    <a className={ props.pagesActive[1] ? "nav-item nav-link active" : "nav-item nav-link" } href="Education">Education</a>
-                    <a className={ props.pagesActive[2] ? "nav-item nav-link active" : "nav-item nav-link" } href="Projects">Projects</a>
+                    <a className={ props.pagesActive[0] ? "nav-item nav-link active" : "nav-item nav-link" } href="/Personal-Website/#/">Home <span className="sr-only">(current)</span></a>
+                    <a className={ props.pagesActive[1] ? "nav-item nav-link active" : "nav-item nav-link" } href="/Personal-Website/#/Education">Education</a>
+                    <a className={ props.pagesActive[2] ? "nav-item nav-link active" : "nav-item nav-link" } href="/Personal-Website/#/Projects">Projects</a>
                 </div>
             </div>
         </nav>
